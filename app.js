5e8f99e6ac0fd1a26001bad945af7a03878ce23f
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-11 08:26:03 +0100

    complete javascript part

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 let countsValue = document.getElementById("counts-value")
+let addBtn = document.getElementById("add-btn")
+let substractBtn = document.getElementById("substract-btn")
 let counts = 0
+
+addBtn.addEventListener("click", function(){
+    counts = counts + 1
+    countsValue.textContent = counts
+})
+
+substractBtn.addEventListener("click", function(){
+    counts = counts - 1
+    countsValue.textContent = counts
+})
